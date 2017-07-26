0a896d29e6b5c8eff018baa2a0946a35d19fb6f7
xtity <kousuke.sima@gmail.com>
2017-07-26 23:31:54 +0900

    Update lib version

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,37 @@
 var webpack = require("webpack");
 
 module.exports = {
-  entry: './src/scripts/index.ts',
+  entry: "./src/scripts/index.ts",
   output: {
-    path: __dirname + '/app/scripts',
-    filename: 'bundle.js',
-		publicPath: '/app/',
+    path: __dirname + "/app/scripts",
+    filename: "bundle.js",
+		publicPath: "/app/",
   },
   module: {
-    preLoaders: [
+    rules: [
       {
         test: /\.tag$/,
         exclude: /node_modules/,
-        loader: 'riotjs-loader',
+        enforce: "pre",
+        loader: "riotjs-loader",
         query: {
-          type: 'typescript'
+          type: "typescript"
         }
-      }
-    ],
-    loaders: [
+      },
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: 'ts-loader'
+        loader: "ts-loader"
       }
-    ]
+    ],
   },
   resolve: {
-      extensions: ['', '.js', '.tag', '.ts']
+      extensions: ["*", ".ts", ".js", ".tag"]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.ProvidePlugin({
-      riot: 'riot'
+      riot: "riot"
     })
   ]
 }
